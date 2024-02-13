4aa7eb83f794de01004985a387ff0630c80fdbcb
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2024-02-14 02:30:52 +0300

    cleanup

--- src/services/nftService.ts
+++ src/services/nftService.ts
@@ -2,19 +2,27 @@ import {Collections, Helius} from "helius-sdk";
 
 const helius = new Helius(import.meta.env.VITE_APP_HELIUS_APIKEY);
 export const createNft = async (name:string,owner:string, description:string,imageUrl:string,fee:number,symbol:string) => {
-  const response = await helius.mintCompressedNft({
-    name:name,
-    symbol:symbol,
-    owner:owner,
-    description:description,
-    imageUrl:imageUrl,
-    sellerFeeBasisPoints:fee
-  });
-  console.log(response.result);
+  try{
+    const response = await helius.mintCompressedNft({
+      name: name,
+      symbol: symbol,
+      owner: owner,
+      description: description,
+      imageUrl: imageUrl,
+      sellerFeeBasisPoints: fee
+    });
+    console.log(response.result);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export const getMints = async () => {
-  const response = await helius.getMintlist({query: Collections.ABC});
-  console.log(response.result);
-  return response.result;
+  try{
+    const response = await helius.getMintlist({query: Collections.ABC});
+    console.log(response.result);
+    return response.result;
+  }catch (error) {
+    console.log(error);
+  }
 }
