248289bd89f2597a2d336ded905aad99491fe109
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2024-02-16 01:09:46 +0300

    fix: mint form required section

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ export const Layout = ({ children }:Props) => {
   return (
     <div className="flex flex-row h-screen bg-gradient-to-tl font-thin tracking-widest from-slate-500 to-purple-950 filter">
       <Sidebar/>
-      <div className="layout-calc ml-auto overflow-y-auto">
+      <div className="layout-calc ml-auto overflow-auto">
         {children}
       </div>
     </div>
--- src/components/MintForm.tsx
+++ src/components/MintForm.tsx
@@ -4,11 +4,11 @@ import {useRef, useState} from "react";
 import {MdCloudUpload} from "react-icons/md";
 
 export const MintForm = () => {
-  const [isUploaded,setIsUploaded] = useState(false)
+  const [isUploaded, setIsUploaded] = useState(false)
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: {errors}
   } = useForm<MintFormProps>()
 
   const imageRef = useRef<HTMLInputElement>(null)
@@ -29,34 +29,70 @@ export const MintForm = () => {
   const onSubmit = (data: MintFormProps) => console.log(data)
 
   return (
-    <form className="flex flex-col text-white gap-3 lg:w-[500px] md:w-[400px] transition-all px-4 py-4 bg-black bg-opacity-20 rounded-lg items-center" onSubmit={handleSubmit(onSubmit)}>
-      <label className="flex flex-row  gap-2 w-full" htmlFor="name">Name<p className="text-red-500">*</p></label>
-      <input className="bg-inherit py-2 px-4 w-full" {...register("name",{required:true})} />
-      {errors.name && errors.name.type === "required" && (
-        <span className="w-full text-end text-red-600 text-opacity-30">Required</span>
-      )}
-      <label className="flex flex-row  gap-2 w-full" htmlFor="symbol">Symbol<p className="text-red-500">*</p></label>
-      <input className="bg-inherit py-2 px-4 w-full" {...register("symbol",{required:true})} />
-      {errors.name && errors.name.type === "required" && (
-        <span className="w-full text-end text-red-600 text-opacity-30">Required</span>
-      )}
-      <label className="flex flex-row  gap-2 w-full" htmlFor="fee">Fee<p className="text-red-500">*</p></label>
-      <input className="bg-inherit py-2 px-4 w-full" {...register("fee",{required:true})} />
-      {errors.name && errors.name.type === "required" && (
-        <span className="w-full text-end text-red-600 text-opacity-30">Required</span>
-      )}
-      <label className="flex flex-row  gap-2 w-full" htmlFor="description">Description<p className="text-red-500">*</p></label>
-      <input className="bg-inherit py-2 px-4 w-full" {...register("description",{required:true})} />
-      {errors.name && errors.name.type === "required" && (
-        <span className="w-full text-end text-red-600 text-opacity-30">Required</span>
-      )}
-      <label className="flex flex-row  gap-2 w-full" htmlFor="image">Image</label>
-      <input accept=".png,.jpg,.jpeg" type="file" ref={imageRef} className="hidden" onChange={()=>uploadImage}/>
-      <button type="button" onClick={()=>onImageUpload()} className="bg-transparent border-dashed border-white border rounded-md flex items-center justify-center py-2 w-full h-12 mt-2">
-        <MdCloudUpload size={32} />
+    <form
+      className="flex flex-col text-white gap-3 lg:w-[500px] md:w-[400px] transition-all px-4 py-4 bg-black bg-opacity-20 rounded-lg items-center"
+      onSubmit={handleSubmit(onSubmit)}>
+      <label className="flex flex-row  gap-2 w-full"
+             htmlFor="name">
+        Name
+        <p className="text-red-500">*</p>
+        {errors.name && errors.name.type === "required" && (
+          <span
+            className="w-full text-end text-red-600 text-opacity-80">Required</span>
+        )}
+      </label>
+      <input
+        className="bg-inherit py-2 px-4 w-full" {...register("name", {required: true})} />
+      <label className="flex flex-row  gap-2 w-full"
+             htmlFor="symbol">
+        Symbol
+        <p
+          className="text-red-500">*</p>
+        {errors.name && errors.name.type === "required" && (
+          <span
+            className="w-full text-end text-red-600 text-opacity-80">Required</span>
+        )}
+      </label>
+      <input
+        className="bg-inherit py-2 px-4 w-full" {...register("symbol", {required: true})} />
+      <label className="flex flex-row  gap-2 w-full"
+             htmlFor="fee">
+        Fee
+        <p
+          className="text-red-500">*</p>
+        {errors.name && errors.name.type === "required" && (
+          <span
+            className="w-full text-end text-red-600 text-opacity-80">Required</span>
+        )}
+      </label>
+      <input
+        className="bg-inherit py-2 px-4 w-full" {...register("fee", {required: true})} />
+      <label className="flex flex-row  gap-2 w-full"
+             htmlFor="description">
+        Description
+        <p
+          className="text-red-500">*</p>
+        {errors.name && errors.name.type === "required" && (
+          <span
+            className="w-full text-end text-red-600 text-opacity-80">Required</span>
+        )}</label>
+      <input
+        className="bg-inherit py-2 px-4 w-full" {...register("description", {required: true})} />
+      <label className="flex flex-row  gap-2 w-full"
+             htmlFor="image">Image</label>
+      <input accept=".png,.jpg,.jpeg" type="file"
+             ref={imageRef} className="hidden"
+             onChange={() => uploadImage}/>
+      <button type="button" onClick={() => onImageUpload()}
+              className="bg-transparent border-dashed border-white border rounded-md flex items-center justify-center py-2 w-full h-12 mt-2">
+        <MdCloudUpload size={32}/>
       </button>
-      {isUploaded && <p className="text-sm font-thin">Image Uploaded (Upload again to change)</p>}
-      <button type="submit" className="bg-white bg-opacity-20 hover:bg-opacity-30 py-2 w-36 mt-2 rounded-md">Create</button>
+      {isUploaded &&
+          <p className="text-sm font-thin">Image Uploaded
+              (Upload again to change)</p>}
+      <button type="submit"
+              className="bg-white bg-opacity-20 hover:bg-opacity-30 py-2 w-36 mt-2 rounded-md">Create
+      </button>
     </form>
   )
 }
