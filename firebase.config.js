fa1b982729f5800260bd0ed45739784aa4b8e8a4
Ali Kagan Yilmaz <kaganylmz12@gmail.com>
2024-03-11 18:23:48 +0300

    fix: mint form

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import {getStorage} from "firebase/storage";
+import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_APP_FIREBASE_APIKEY,
@@ -12,3 +13,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
--- src/components/MintForm.tsx
+++ src/components/MintForm.tsx
@@ -2,8 +2,22 @@ import {MintFormProps} from "../utils/types.ts";
 import {useForm} from "react-hook-form";
 import {useRef, useState} from "react";
 import {MdCloudUpload} from "react-icons/md";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "firebase/storage";
+import {v4 as uuidv4} from 'uuid';
+import {collection, addDoc} from "firebase/firestore";
+import {storage, db} from "../../firebase.config.js";
+import {createNft} from "../services/nftService.ts";
+import {useWeb3React} from "@web3-react/core";
 
 export const MintForm = () => {
+  const {connector, hooks} = useWeb3React();
+  const {useSelectedAccount} = hooks;
+  const account = useSelectedAccount(connector);
+  const [downloadUrl, setDownloadUrl] = useState<string>("")
   const [isUploaded, setIsUploaded] = useState(false)
   const {
     register,
@@ -17,21 +31,55 @@ export const MintForm = () => {
       imageRef.current.click()
     }
   }
-  const uploadImage = () => {
+
+  const uploadImage = (file: File) => {
     try {
-      setIsUploaded(true)
+      const id = uuidv4();
+      const storageRef = ref(storage, 'images/' + id);
+      uploadBytes(storageRef, file).then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((downloadURL) => {
+          setDownloadUrl(downloadURL)
+        });
+      });
+      setIsUploaded(true);
+
     } catch (error) {
       setIsUploaded(false)
       alert("Something went wrong")
     }
   }
 
-  const onSubmit = (data: MintFormProps) => console.log(data)
+  const postFirestore = async (data: MintFormProps) => {
+    await addDoc(collection(db, "nfts"), {
+      name: data.name,
+      symbol: data.symbol,
+      amount: data.fee,
+      description: data.description,
+      image: downloadUrl
+    });
+  }
+  const onSubmit = (data: MintFormProps) => {
+      if (account) {
+        createNft(
+          data.name,
+          account,
+          data.description,
+          downloadUrl,
+          +data.fee,
+          data.symbol
+        )
+          .then(() => postFirestore(data)).catch((error) => {
+          console.log(error)
+        });
+      } else {
+        alert("Connect Wallet")
+      }
+  }
 
   return (
     <form
       className="flex flex-col text-white gap-3 w-full lg:w-[500px] md:w-[400px] transition-all px-4 py-4 bg-black bg-opacity-20 rounded-lg items-center"
-      onSubmit={handleSubmit(onSubmit)}>
+      onSubmit={handleSubmit(()=>onSubmit)}>
       <label className="flex flex-row  gap-2 w-full"
              htmlFor="name">
         Name
@@ -82,7 +130,7 @@ export const MintForm = () => {
              htmlFor="image">Image</label>
       <input accept=".png,.jpg,.jpeg" type="file"
              ref={imageRef} className="hidden"
-             onChange={() => uploadImage}/>
+             onChange={(e) => e.target.files && e.target.files[0] && uploadImage(e.target.files[0])}/>
       <button type="button" onClick={() => onImageUpload()}
               className="bg-transparent border-dashed border-white border rounded-md flex items-center justify-center py-2 w-full h-12 mt-2">
         <MdCloudUpload size={32}/>
